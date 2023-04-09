77cf741c3038b266cd54b1fd3264c64ac5e532c5
Visa Pollari <visa.pollari@gmail.com>
2023-04-09 14:55:20 +0300

    get average

--- database/database.py
+++ database/database.py
@@ -20,3 +20,26 @@ def putItem(year, guild, score):
             "timestamp": timestamp,
         }
     )
+
+
+def getAverage(day: str, guild: str = None):
+    table = dynamodb.Table(os.getenv("DYNAMODB_TABLE_NAME"))
+    if guild:
+        response = table.query(
+            KeyConditionExpression="partition_key = :pk",
+            ExpressionAttributeValues={
+                ":pk": f"{day}::{guild}",
+            },
+        )
+    else:
+        response = table.query(
+            KeyConditionExpression="partition_key = :pk",
+            ExpressionAttributeValues={
+                ":pk": f"{day}",
+            },
+        )
+    items = response["Items"]
+    if len(items) == 0:
+        return None
+    else:
+        return sum([item["score"] for item in items]) / len(items)
